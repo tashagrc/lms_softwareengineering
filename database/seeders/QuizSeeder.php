08c2fecdc6b22c77a8c1abf56c9aefd2ebe518d3
Natasha Grace <natashagradika@gmail.com>
2023-06-02 12:06:57 +0700

    seeding done

--- database/seeders/QuizSeeder.php
+++ database/seeders/QuizSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class QuizSeeder extends Seeder
 {
@@ -13,6 +14,34 @@ class QuizSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('quizzes')->insert([
+            [
+                'QuizID' => 1,
+                'QuizTitle' => 'Quiz 1',
+                'QuizDate' => '2023-06-01',
+                'QuizStart' => null,
+                'QuizEnd' => null,
+                'QuizPrepare' => null,
+                'SessionID' => 1,
+                'CourseID' => 1,
+                'ClassroomID' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'QuizID' => 2,
+                'QuizTitle' => 'Quiz 2',
+                'QuizDate' => '2023-06-02',
+                'QuizStart' => null,
+                'QuizEnd' => null,
+                'QuizPrepare' => null,
+                'SessionID' => 2,
+                'CourseID' => 1,
+                'ClassroomID' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            // Add more quiz records as needed
+        ]);
     }
 }
