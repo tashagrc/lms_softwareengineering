b844029e03782cabce354586c1eb0a8f431ba608
Michelle Angela Aryanto <michelle.aryanto@binus.ac.id>
2023-06-07 18:08:24 +0700

    feat: pake createQuiz1.blade.php lama dulu aja

--- resources/views/createQuiz1.blade.php
+++ resources/views/createQuiz1.blade.php
@@ -1,4 +1,3 @@
-<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
 @extends('layouts.navbarTwo')
 
 <!DOCTYPE html>
@@ -13,73 +12,68 @@
 <body>
 
 @section('container')
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <div class="content-middle d-flex justify-center">
-                <div class="steps my-4">
-                    <div class="step-layout">
-                        <a href="" class="step-layout-button-diff" id="first-step">
-                            <p class="spacing" id="number1">1</p>
-                            <p class="spacing" id="desc1">Class</p>
-                        </a>
-                        <p class="spacing" id="arrow-diff">></p>
-                    </div>
-                    <div class="step-layout">
-                        <div class="step-layout-button" id="second-step">
-                            <p class="spacing" id="number2">2</p>
-                            <p class="spacing" id="desc2">Subject</p>
-                        </div>
-                        <p class="spacing" id="arrow">></p>
-                    </div>
-                    <div class="step-layout">
-                        <div href="" class="step-layout-button" id="third-step">
-                            <p class="spacing" id="number3">3</p>
-                            <p class="spacing" id="desc3">Topic</p>
-                        </div>
-                        <p class="spacing" id="arrow">></p>
-                    </div>
-                    <div class="step-layout">
-                        <div href="" class="step-layout-button" id="fourth-step">
-                            <p class="spacing" id="number4">4</p>
-                            <p class="spacing" id="desc4">Information</p>
-                        </div>
-                        <p class="spacing" id="arrow">></p>
-                    </div>
-                    <div class="step-layout">
-                        <div href="" class="step-layout-button" id="fifth-step">
-                            <p class="spacing" id="number5">5</p>
-                            <p class="spacing" id="desc5">Questions</p>
-                        </div>
-                        <p class="spacing" id="arrow">></p>
-                    </div>
-                    <div class="step-layout">
-                        <div href="" class="step-layout-button" id="sixth-step">
-                            <p class="spacing" id="number6">6</p>
-                            <p class="spacing" id="desc6">Upload</p>
-                        </div>
-                    </div>
+    <div class="content-middle">
+        <div class="steps">
+            <div class="step-layout">
+                <a href="" class="step-layout-button-diff" id="first-step">
+                    <p class="spacing" id="number1">1</p>
+                    <p class="spacing" id="desc1">Class</p>
+                </a>
+                <p class="spacing" id="arrow-diff">></p>
+            </div>
+            <div class="step-layout">
+                <div class="step-layout-button" id="second-step">
+                    <p class="spacing" id="number2">2</p>
+                    <p class="spacing" id="desc2">Subject</p>
                 </div>
-                <div class="content">
-                    <h1 class="content-title">Select class</h1>
-                    <div class="content">
-                        <div class="dropdown">
-                            <button class="dropdown-button">Select</button>
-                            <div class="dropdown-content">
-                                <a href="#">Link 1</a>
-                                <a href="#">Link 2</a>
-                                <a href="#">Link 3</a>
-                            </div>
-                        </div>
-                    </div>
+                <p class="spacing" id="arrow">></p>
+            </div>
+            <div class="step-layout">
+                <div href="" class="step-layout-button" id="third-step">
+                    <p class="spacing" id="number3">3</p>
+                    <p class="spacing" id="desc3">Topic</p>
                 </div>
-                <div class="action-button">
-                    <a href=""><button class="savecontinue">Save & Continue</button></a>
+                <p class="spacing" id="arrow">></p>
+            </div>
+            <div class="step-layout">
+                <div href="" class="step-layout-button" id="fourth-step">
+                    <p class="spacing" id="number4">4</p>
+                    <p class="spacing" id="desc4">Information</p>
+                </div>
+                <p class="spacing" id="arrow">></p>
+            </div>
+            <div class="step-layout">
+                <div href="" class="step-layout-button" id="fifth-step">
+                    <p class="spacing" id="number5">5</p>
+                    <p class="spacing" id="desc5">Questions</p>
+                </div>
+                <p class="spacing" id="arrow">></p>
+            </div>
+            <div class="step-layout">
+                <div href="" class="step-layout-button" id="sixth-step">
+                    <p class="spacing" id="number6">6</p>
+                    <p class="spacing" id="desc6">Upload</p>
                 </div>
             </div>
         </div>
+        <div class="content">
+            <h1 class="content-title">Select class</h1>
+            <div class="content">
+                <div class="dropdown">
+                    <button class="dropdown-button">Select</button>
+                    <div class="dropdown-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="action-button">
+            <a href=""><button class="savecontinue">Save & Continue</button></a>
+        </div>
     </div>
-</div>
+
 
 
 </body>
