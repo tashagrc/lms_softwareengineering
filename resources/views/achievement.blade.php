35f07ace405b048fb2f2041e0889c0b33f307458
Natasha Grace <natashagradika@gmail.com>
2023-06-11 16:07:27 +0700

    navbar 4 rapiin

--- resources/views/achievement.blade.php
+++ resources/views/achievement.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ReadRacoon</title>
-    <link rel="stylesheet" href="css/achievement.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/achievement.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}">
 </head>
 <body>
