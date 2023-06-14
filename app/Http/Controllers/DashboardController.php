343469317bf249e25b3a651c33a057a745bde9e1
Michelle Angela Aryanto <michelle.aryanto@binus.ac.id>
2023-06-14 12:04:57 +0700

    feat: progress bar gamified

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Badges;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function show(){
-
-        return view('dashboard');
+        $userXP = User::find(1)->value('UserXP');
+        return view('dashboard', compact('userXP'));
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Badges;
+
 
 class UserController extends Controller
 {
     function editProfile() {
         $users = User::all()->get(0); // should be current user
-        return view('profile.edit', compact('users'));
+        $userXP = User::find(1)->value('UserXP');
+        return view('profile.edit', compact('users', 'userXP'));
     }
 }
